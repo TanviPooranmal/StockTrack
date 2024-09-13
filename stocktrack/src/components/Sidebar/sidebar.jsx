d4797ee85e510aa974df912b9f7fc94bd5138725
Tanvi Pooranmal Meena <96572616+TanviPooranmal@users.noreply.github.com>
2024-09-13 15:50:41 +0530

    Create Sidebar component

---
+++ stocktrack/src/components/Sidebar/sidebar.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import { ReactComponent as SidebarTabsSVG } from '../../assets/Menu_light.svg';
+import { ReactComponent as LogoSVG } from '../../assets/Logo.svg';
+
+const Sidebar = () => {
+  return (
+    <Box
+      sx={{
+        width: '12.5em',
+        height: '100vh',
+        backgroundColor: '#FFFFFF',
+        boxShadow: '2px 0px 5px rgba(0, 0, 0, 0.1)',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        boxSizing: 'border-box',
+      }}
+    >
+      {/* Top Section */}
+      <div>
+        {/* Logo */}
+        <Box
+          sx={{
+            height: '4.5em',
+            width: '100%',
+            padding: '1.5em',
+            backgroundColor: '#ECECEC',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: '1.5rem',
+            fontWeight: 'bold',
+            textAlign: 'center',
+            gap: '0.375em',
+            boxSizing: 'border-box',
+            margin: 0,
+          }}
+        >
+          <LogoSVG style={{ height: '100%', width: 'auto' }} />
+        </Box>
+
+        {/* SVG for Tab List */}
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            height: '22em',
+            overflow: 'hidden',
+          }}
+        >
+          <SidebarTabsSVG style={{ width: '100%', height: 'auto' }} />
+        </Box>
+      </div>
+    </Box>
+  );
+};
+
+export default Sidebar;
--- stocktrack/src/pages/Dashboard.js
+++ stocktrack/src/pages/Dashboard.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import TopBar from '../components/Topbar/topbar';
+import {
+  TopBar,
+  Sidebar
+} from '../components';
 import styles from '../css/dashboard.module.css';
 
 const Dashboard = () => {
   return (
     <div className={styles.dashboard}>
-      <TopBar />
-      <div className={styles.content}>
-        <h1>Dashboard</h1>
-        {/* Add more content or components for the dashboard here */}
+      <Sidebar />
+      <div className={styles.mainContent}>
+        <TopBar />
+        <div className={styles.content}>
+          <h1>Dashboard</h1>
+          {/* Add more content or components for the dashboard here */}
+        </div>
       </div>
     </div>
   );
