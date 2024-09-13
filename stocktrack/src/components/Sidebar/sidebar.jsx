da131f2380eefa96bff74dc3987761bea9508520
Tanvi Pooranmal Meena <96572616+TanviPooranmal@users.noreply.github.com>
2024-09-13 22:51:59 +0530

    Remove unused variable

--- stocktrack/src/components/Sidebar/sidebar.jsx
+++ stocktrack/src/components/Sidebar/sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { ReactComponent as SidebarTabsSVG } from '../../assets/Menu_light.svg';
 import { ReactComponent as LogoSVG } from '../../assets/Logo.svg';
 
